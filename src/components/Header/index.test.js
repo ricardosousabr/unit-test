e480fa0771bc4c06f6ea88af82e97f4b2d6c323a
ricardo <ricardo.br.pi@gmail.com>
2023-01-12 21:55:06 -0300

    test: update the tests

--- src/components/Header/index.test.js
+++ src/components/Header/index.test.js
@@ -1,5 +1,32 @@
 const soma = require('./index');
 
+// True, False, Null, Undefined
+test('Null', () => {
+  const n = null
+
+  expect(n).toBeNull();
+})
+
+test('undefined', () => {
+  const u = 1
+
+  expect(u).toBeDefined();
+})
+
+test('true', () => {
+  const t = true
+
+  expect(t).toBeTruthy();
+})
+
+test('true', () => {
+  const f = false
+
+  expect(f).toBeFalsy();
+})
+
+
+//Number
 test('Soma de 4 + 4 deve ser 8', () => {
   expect(soma(4, 4)).toBe(8);
  }
@@ -14,3 +41,30 @@ test('Soma de -4 + 4 deve ser 0', () => {
   expect(soma(5, 4)).toBeGreaterThan(5);
  }
 )
+
+
+//String
+test('existe r em Christoph', () => {
+  expect('Christoph').toMatch(/r/);
+})
+
+test('não existe r em Christoph', () => {
+  expect('Christoph').not.toMatch(/y/);
+})
+
+//Array
+const shoppingList = [
+  'fraldas',
+  'kleenex',
+  'sacos de lixo',
+  'papel toalha',
+  'leite',
+];
+
+test('a lista de compras tem leite nela', () => {
+  expect(shoppingList).toContain('leite');
+});
+
+test('a lista de compras tem leite nela', () => {
+  expect(shoppingList).not.toContain('  ');
+});
